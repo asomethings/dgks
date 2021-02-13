36e65268bf7f2ee0bd5b8134f0f64e7c3f406391
Ewan <asomethings@kakao.com>
2021-02-13 21:21:27 +0000

    feat(core): create engine options interface

---
+++ packages/core/src/interfaces/engine-options.interface.ts
@@ -0,0 +1,18 @@
+import { KeyOption } from './key-option.interface'
+
+/**
+ * Engine options
+ */
+export interface EngineOptions extends KeyOption {
+  /**
+   * ID of openapi url
+   * @example 1611000
+   */
+  id: string | number
+
+  /**
+   * Service name of openapi
+   * @example BldRgstService_v2
+   */
+  name: string
+}
